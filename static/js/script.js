aa2bf3a98b79de134968c0a5a19f510fce12f875
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-19 23:01:25 +0000

    Hamburger button id chagned, js script added to fix button not working issue

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,11 @@
 /**
  * This code fill change hamburegr button from lines to cross sign
  */
-$(document).ready(function(){
-	$('#hamburger-button').click(function(){
+$(document).ready(function () {
+	$('#hamburger-button').click(function () {
+		$(this).toggleClass('open');
+	});
+	$('#hamburger-button-medium').click(function () {
 		$(this).toggleClass('open');
 	});
 });
