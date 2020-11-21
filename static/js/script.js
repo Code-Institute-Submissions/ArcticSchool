bf8bf2769db8e1968e77ad9eeb318601c6986b03
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-21 17:23:57 +0000

    Scroll animation added to scroll-icon button, code snippet used from www.w3schools.com

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,30 @@
-/**
- * This code fill change hamburegr button from lines to cross sign
- */
 $(document).ready(function () {
+	/**
+	* This function fill change hamburegr button from lines to cross sign on small devices
+	*/
 	$('#hamburger-button').click(function () {
 		$(this).toggleClass('open');
 	});
+	/**
+	* This function fill change hamburegr button from lines to cross sign on medium devices
+	*/
 	$('#hamburger-button-medium').click(function () {
 		$(this).toggleClass('open');
+	});
+
+	/**
+	 * This fucntion was found on https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section1, traget - element changed also added -=57px to scrollTop function to adjust navigation size to scrolled elemenet
+	 */
+	$(".scroll-icon").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			let hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top -= 57
+			}, 800, function () {
+				window.location.hash = hash;
+			});
+		}
 	});
 });
