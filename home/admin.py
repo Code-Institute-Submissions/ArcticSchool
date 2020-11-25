6b6b749896547eecbc340f5e84a91e7ed4e5a120
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-25 13:53:14 +0000

    Models registered to admin page

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Levels
+from .models import Levels, Lesson, Social
 # Register your models here.
 
 
 admin.site.register(Levels)
+admin.site.register(Lesson)
+admin.site.register(Social)
