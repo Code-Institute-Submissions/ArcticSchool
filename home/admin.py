85b59f702908f76bd3596cddd681695e1dddd00f
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-25 13:12:57 +0000

    Fixtures and migrations for home level cards test created. Passed

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Levels
 # Register your models here.
+
+
+admin.site.register(Levels)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Levels(models.Model):
+    class levelChoices(models.TextChoices):
+        Level_1 = '1',
+        Level_2 = '2',
+        Level_3 = '3',
+        Level_4 = '4'
+
+
+    title = models.TextField(max_length=40)
+    level = models.CharField(choices=levelChoices.choices, default=levelChoices.Level_1, max_length=1)
+    description = models.TextField(max_length=130)
