d4ea10ab4a4a096d0f6ac810dc14eb1de044218d
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-25 14:16:25 +0000

    Models importent and passed to index view as content

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Lesson, Levels, Social
 
 # Create your views here.
+
+
 def index(request):
     """ A view to return index page """
-    return render(request, 'home/index.html')
+
+    lesson = Lesson.objects.all()
+    levels = Levels.objects.all()
+    social = Social.objects.all()
+
+    context = {
+        'lessons': lesson,
+        'levels': levels,
+        'socials': social,
+    }
+
+    return render(request, 'home/index.html', context)
