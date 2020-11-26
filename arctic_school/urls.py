46ee3af348085225158e643cc9814552081f734d
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-26 10:49:02 +0000

    My-account app removed

--- arctic_school/urls.py
+++ arctic_school/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('my-account/', include('my_account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my_account/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def my_account(request):
-    """ A view to return my account page """
-    return render(request, 'my_account/login-register.html')
