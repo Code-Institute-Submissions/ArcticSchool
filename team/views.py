9ec960c7ea04bd2e25c7df10cb02b35101e43931
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-26 12:30:47 +0000

    Content from models added to view

--- team/views.py
+++ team/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import InstructorProfile
 # Create your views here.
 
 
 def team(request):
     """ A view to return team page """
-    return render(request, 'team/team.html')
+
+    instructors = InstructorProfile.objects.all()
+    context = {
+        'instructors': instructors,
+    }
+
+    return render(request, 'team/team.html', context)
