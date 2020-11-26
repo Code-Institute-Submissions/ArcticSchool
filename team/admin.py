2b60cf558345eb623afc001a4952bafe8c3d33fd
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-26 11:49:57 +0000

    Model registered with django admin

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import InstructorProfile
 
 # Register your models here.
+
+
+class InstructorProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'age',
+        'about',
+        'style',
+        'trick',
+        'qualifications',
+        'image_url',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(InstructorProfile, InstructorProfileAdmin)
