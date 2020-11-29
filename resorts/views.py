6e395b0d3e49c3e61e11633310fe85563c811216
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-29 19:49:33 +0000

    Basic resorts template created. Resorts card section created

--- resorts/views.py
+++ resorts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Resort
 # Create your views here.
 
 
 def resorts(request):
     """ A view to return team page """
 
-    return render(request, 'resorts/resorts.html')
+    resorts = Resort.objects.all()
+
+    context = {
+        'resorts': resorts,
+    }
+
+    return render(request, 'resorts/resorts.html', context)
