5fa41d58ad372059768b0c8e2c5f7f09eb9a3a2f
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-11-29 17:53:13 +0000

    Team app - Instructors model unwanted variable removed. Image field directory added

--- team/models.py
+++ team/models.py
@@ -10,8 +10,7 @@ class InstructorProfile(models.Model):
     name = models.CharField(max_length=254)
     age = models.CharField(max_length=2, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to="instructors",null=True, blank=True)
 
     def __str__(self):
         return self.name
