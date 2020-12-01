42612d4da4962c7d4c9d3d0398dd1740c26b0507
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-01 22:16:25 +0000

    Socail icons model imported

--- resorts/views.py
+++ resorts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Resort
+from home.models import SocialIcon
 # Create your views here.
 
 
@@ -7,9 +8,11 @@ def resorts(request):
     """ A view to return team page """
 
     resorts = Resort.objects.all()
+    social =  SocialIcon.objects.all()
 
     context = {
         'resorts': resorts,
+        'socials': social,
     }
 
     return render(request, 'resorts/resorts.html', context)
