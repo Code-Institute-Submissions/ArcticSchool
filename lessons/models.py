4195dd7f5d5ee1ebfa432394a8ceea9a84cf939f
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-06 23:42:39 +0000

    Promo text field updated criteria

--- lessons/models.py
+++ lessons/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    promo_text = models.TextField()
+    promo_text = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.category
@@ -28,7 +28,7 @@ class Lesson(models.Model):
     end_date = models.DateField()
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
-    participants  =models.IntegerField()
+    participants = models.IntegerField()
     place = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
