4e164f24989a4159606b009aac86f0a8680b40bf
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-06 23:37:45 +0000

    Categories and Lessons models imported to lessons view

--- lessons/views.py
+++ lessons/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Lesson
 # Create your views here.
 
 
 def lessons(request):
     """ A view to return lessons page """
 
+    categories = Category.objects.all()
+    lessons = Category.objects.all()
+
     context = {
+        'categories': categories,
+        'lessons': lessons
     }
 
     return render(request, 'lessons/lessons.html', context)
