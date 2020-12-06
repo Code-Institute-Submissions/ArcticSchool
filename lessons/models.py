18fd4a5fbea526906a57eaafd3b3bca1b9f83a9c
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-06 23:18:44 +0000

    Category and Lesson models created

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.category
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Lesson(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField(auto_now=False, auto_now_add=False)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False)
+    participants  =models.IntegerField(max_length=3, default='')
+    place = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
