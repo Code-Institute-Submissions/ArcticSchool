ccc43d74c94e5cd6873b6b884a8e8ac71a94509a
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-06 23:18:26 +0000

    Models registered on admin site and certain fields display lists prepared

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Category, Lesson
 
-# Register your models here.
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class LessonsAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'start_date',
+        'end_date',
+        'start_time',
+        'end_time',
+        'participants',
+        'place',
+        'price',
+        'image'
+    )
+
+
+admin.site.register(Category, CategoriesAdmin)
+admin.site.register(Lesson, LessonsAdmin)
