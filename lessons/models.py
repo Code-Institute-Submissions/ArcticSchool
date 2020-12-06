0fe7bb2801ecb9edd5efc361dbe8f7a5825472f5
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-06 23:26:07 +0000

    Verbose name meta class added

--- lessons/models.py
+++ lessons/models.py
@@ -5,6 +5,9 @@ from django.db.models.fields import CharField
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
