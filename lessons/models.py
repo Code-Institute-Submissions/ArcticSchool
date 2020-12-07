6092e3f24d7fe3748a62f3b6d709b8f0e1aec188
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-07 19:37:25 +0000

    Lessons model level and resort field added from importet home and resorts models

--- lessons/models.py
+++ lessons/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.db.models.fields import CharField
-
-# Create your models here.
+from home.models import LevelCard
+from resorts.models import Resort
 
 
 class Category(models.Model):
@@ -20,16 +19,19 @@ class Category(models.Model):
 
 
 class Lesson(models.Model):
-    category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.PROTECT)
+    level = models.ForeignKey(
+        LevelCard, null=True, blank=True, on_delete=models.PROTECT)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
     participants = models.IntegerField()
-    place = models.CharField(max_length=254, null=True, blank=True)
+    resort = models.ForeignKey(
+        Resort, null=True, blank=True, on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
