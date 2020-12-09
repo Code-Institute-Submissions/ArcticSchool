a30fcc7dcf9b77d7061b6de5ae8699f713cdf6b0
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-09 18:56:31 +0000

    Filtered category view created to show lessons for 'clicked' category on lessons page

--- lessons/views.py
+++ lessons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Lesson
 # Create your views here.
 
@@ -7,11 +7,21 @@ def lessons(request):
     """ A view to return lessons page """
 
     categories = Category.objects.all()
-    lessons = Category.objects.all()
-
     context = {
         'categories': categories,
-        'lessons': lessons
     }
 
     return render(request, 'lessons/lessons.html', context)
+
+
+def category_result(request, category_name):
+
+    lessons = Lesson.objects.all()
+    lessons = lessons.filter(category__name=category_name)
+
+    context = {
+        'lessons': lessons,
+        'category_name': category_name,
+    }
+
+    return render(request, 'lessons/lessons-cat.html', context)
