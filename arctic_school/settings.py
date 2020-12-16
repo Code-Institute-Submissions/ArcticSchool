d482525dcde97899687756ca82c3153af8bfae60
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-16 21:29:47 +0000

    Local host added, requirements, updated

--- arctic_school/settings.py
+++ arctic_school/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+t&2u&_0&tm!bos(w4=pfqh45$23izhd)ll+qdjn#%y7n_5ov1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arcticschool.heroku.com','localhost']
 
 
 # Application definition
@@ -108,12 +108,17 @@ WSGI_APPLICATION = 'arctic_school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
