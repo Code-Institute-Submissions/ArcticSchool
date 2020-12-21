e15312d8338b33d1396036ab06ab2a87c0ef4381
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-21 21:39:46 +0000

    Removed secret key and set debug

--- arctic_school/settings.py
+++ arctic_school/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+t&2u&_0&tm!bos(w4=pfqh45$23izhd)ll+qdjn#%y7n_5ov1'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['arcticschool.herokuapp.com', 'localhost', '127.0.0.1']
 
