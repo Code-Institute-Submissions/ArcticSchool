7a2d23451c0b86bbb1e9ddb530e783fa459cd738
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-23 17:17:38 +0000

    'x' removed to clear heroku error

--- arctic_school/settings.py
+++ arctic_school/settings.py
@@ -178,7 +178,7 @@ if 'BUCKET_S3' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'x
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
