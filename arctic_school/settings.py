7bf0f370c2a336c0945870cdb479539b375801ae
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-23 17:34:39 +0000

    Bucket name spelling correted

--- arctic_school/settings.py
+++ arctic_school/settings.py
@@ -170,7 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'BUCKET_S3' in os.environ:
     # Bucket S3 Congig
-    AWS_STORAGE_BUCKET_NAME = "arctischool"
+    AWS_STORAGE_BUCKET_NAME = "arcticschool"
     AWS_S3_REGION_NAME = "eu-west-2"
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
