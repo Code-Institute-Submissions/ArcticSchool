37f89e1ed38cb4c574fa3ef66ccb73cd1142b097
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2020-12-23 17:38:24 +0000

    Cache control added

--- arctic_school/settings.py
+++ arctic_school/settings.py
@@ -169,6 +169,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'BUCKET_S3' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket S3 Congig
     AWS_STORAGE_BUCKET_NAME = "arcticschool"
     AWS_S3_REGION_NAME = "eu-west-2"
