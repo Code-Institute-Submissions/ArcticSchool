90fed9c4cd927187a07e2086a0c0bc827dd8cb74
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-02 17:51:35 +0000

    Quote model registered in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Quote
 
-# Register your models here.
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'description',
+    )
+
+    ordering = ('author',)
+
+
+admin.site.register(Quote, QuoteAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.aggregates import Max
 
-# Create your models here.
+
+class Quote(models.Model):
+    description = models.TextField(max_length=1024)
+    author = models.CharField(max_length=124)
+
+    def __str__(self):
+        return self.author
