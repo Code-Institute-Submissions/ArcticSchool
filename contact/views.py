1dcffa0cfb133ca86314eb8011b72666a1203f71
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-02 21:17:16 +0000

    Quotes changed to 1 random quote

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,17 @@
+import random
+from django.db.models import query
 from django.shortcuts import render
-from home.models import SocialIcon
+from .models import QuoteText
 
 
 def contact(request):
-    """ A view to return contact page """
-
-    social = SocialIcon.objects.all()
+    """ A view to return contact page and quotes """
+    quote = list(QuoteText.objects.all())
+    quote = random.sample(quote, 1)
+    quote = random.choice(quote)
 
     context = {
-        'socials': social,
+        'quote': quote,
     }
 
     return render(request, 'contact/contact.html', context)
