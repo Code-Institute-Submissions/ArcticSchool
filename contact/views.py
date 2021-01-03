2f0f107d18948722d12ce2b729b2752fd4614b83
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-03 11:49:26 +0000

    Contact form promo text added.

--- contact/views.py
+++ contact/views.py
@@ -7,12 +7,4 @@ from .models import QuoteText
 def contact(request):
     """ A view to return contact page and quotes """
 
-    quote = list(QuoteText.objects.all())
-    quote = random.sample(quote, 1)
-    quote = random.choice(quote)
-
-    context = {
-        'quote': quote,
-    }
-
-    return render(request, 'contact/contact.html', context)
+    return render(request, 'contact/contact.html')
