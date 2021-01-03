5bf942fa3fbbdc3c75228372948293947ff33cc9
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-03 10:12:40 +0000

    Contact quote container and text style applied

--- contact/views.py
+++ contact/views.py
@@ -6,6 +6,7 @@ from .models import QuoteText
 
 def contact(request):
     """ A view to return contact page and quotes """
+
     quote = list(QuoteText.objects.all())
     quote = random.sample(quote, 1)
     quote = random.choice(quote)
