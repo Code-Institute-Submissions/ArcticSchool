7f26f849e1b7432d29048dc4ed9fa2eb3a323c89
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-04 22:56:23 +0000

    Variables name changed

--- arctic_school/settings.py
+++ arctic_school/settings.py
@@ -201,12 +201,13 @@ if 'BUCKET_S3' in os.environ:
 # Gmail sending real e-mail settings
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = "arcticschool@gmail.com"
+    DEFAULT_FROM_EMAIL = "arcticschoolmail@gmail.com"
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend’'
     EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
+    EMAIL_PORT = 587
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
