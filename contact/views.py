5b27bbf0b217cc997d2ca245e149c5b15521271a
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-05 22:13:08 +0000

    Email message change from plain to .html file version with variables from forom

--- contact/views.py
+++ contact/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from .forms import ContactForm
 from django.contrib import messages
+from django.template.loader import render_to_string
 
 
 def contact(request):
@@ -16,11 +17,15 @@ def contact(request):
             from_email = contact_form.cleaned_data['from_email']
             subject = contact_form.cleaned_data['subject']
             message = contact_form.cleaned_data['message']
+            html_msg = render_to_string('emails/email.html', {'name': name, 'message':message})
             try:
                 send_mail(subject, message, settings.EMAIL_HOST_USER, [
-                          from_email, settings.EMAIL_HOST_USER], fail_silently=False)
+                          from_email, settings.EMAIL_HOST_USER],
+                          html_message=html_msg, fail_silently=False)
+
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('/contact',
-                            messages.success(request, 'Dear ' + name.title() + ', thanks for reaching out! We will answer in lightning speed.'))
+                            messages.success(request, 'Dear ' + name.title() +
+                                             ', thanks for reaching out! We will answer in lightning speed.'))
     return render(request, "contact/contact.html", {'contact_form': contact_form})
