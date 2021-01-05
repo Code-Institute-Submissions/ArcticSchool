5a1b57a99d1acec6e338d9e190a83f5cfd78bd00
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-05 14:59:25 +0000

    Success url and view removed. Replaced with messages

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.conf import settings
 from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
@@ -12,17 +13,14 @@ def contact(request):
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             name = contact_form.cleaned_data['name']
-            email = contact_form.cleaned_data['email']
+            from_email = contact_form.cleaned_data['from_email']
             subject = contact_form.cleaned_data['subject']
             message = contact_form.cleaned_data['message']
             try:
-                send_mail(subject,
-                          message, settings.EMAIL_HOST_USER, [email], fail_silently=False)
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [
+                          from_email, settings.EMAIL_HOST_USER], fail_silently=False)
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
-            return redirect('success')
+
+            messages.success(request, 'Dear ' + name.capitalize() + ', thanks for reaching out!')
     return render(request, "contact/contact.html", {'contact_form': contact_form})
-
-
-def successView(request):
-    return HttpResponse('Success! Thank you for your message.')
