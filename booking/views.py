ec51743e03afaf96b4b694dccf5ca56d50686ce0
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-11 17:49:21 +0000

    You may like component inlcuded

--- booking/views.py
+++ booking/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from home.models import SocialIcon
+from lessons.models import Lesson
+import random
 
 
 def booking(request):
     """ A view to return booking page """
 
     social = SocialIcon.objects.all()
+    lessons = list(Lesson.objects.all())
+    random_lessons = random.sample(lessons, 12)
 
     context = {
         'socials': social,
+        'lessons': random_lessons,
     }
 
     return render(request, 'booking/booking.html', context)
