6d0c6a8bc35d4ca9f7c32bf691b5c90d73e07fc0
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-13 20:57:22 +0000

    Book now view and url added

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.booking, name='booking'),
+    path('add/<lesson_id>', views.add_to_booking, name='add_to_booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from home.models import SocialIcon
 from lessons.models import Lesson
 import random
@@ -11,9 +11,32 @@ def booking(request):
     lessons = list(Lesson.objects.all())
     random_lessons = random.sample(lessons, 12)
 
+    if 'bag' in request.session:
+        bag = request.session.get('bag')
+        print( bag )
+    else:
+        bag = []
+
     context = {
         'socials': social,
         'lessons': random_lessons,
+        'bag':bag,
     }
 
     return render(request, 'booking/booking.html', context)
+
+
+def add_to_booking(request, lesson_id):
+    """ Add a quantity of the specified lesson to the booking_bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if lesson_id in list(bag.keys()):
+        print('Lesson already in bag')
+    else:
+        bag[lesson_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return render(request, 'booking/booking.html')
