2a5965a6ff1b003a12d55949b737f560cccf8399
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-14 23:44:16 +0000

    Remove lesson from booking bag url added

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.booking, name='booking'),
     path('add/<lesson_id>', views.add_to_booking, name='add_to_booking'),
+    path('remove/<lesson_id>', views.remove_from_booking,
+         name='remove_from_booking'),
 ]
