53937ec43a8bdc3c9ef2c8050522471015c4e32c
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-16 00:21:05 +0000

    Clear booking bag url added

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/<lesson_id>', views.add_to_booking, name='add_to_booking'),
     path('remove/<lesson_id>', views.remove_from_booking,
          name='remove_from_booking'),
+    path('clear/', views.clear_booking, name='clear_booking'),
 ]
