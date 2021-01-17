243b7474270cbadd221a670d22d2fc3ccb2063d8
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-17 21:55:33 +0000

    Stripe variables added

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
 from checkout.forms import OrderForm
+from django.conf import settings
 from django.shortcuts import redirect, render, reverse
 from django.contrib import messages
 from home.models import SocialIcon
@@ -13,11 +14,14 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     social = SocialIcon.objects.all()
+    public_key = settings.STRIPE_PUBLIC_KEY
+    secret_key = settings.STRIPE_SECRET_KEY
 
     context = {
         'order_form': order_form,
-        'socials':social,
-
+        'socials': social,
+        'stripe_public_key': public_key,
+        'client_secret': secret_key,
     }
 
     return render(request, template, context)
