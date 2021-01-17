fe18f96e637f3b8fa8b1724d1ab8e0b080fb94bd
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-17 11:33:27 +0000

    Message style changed. Social icons model included

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,22 @@
 from checkout.forms import OrderForm
 from django.shortcuts import redirect, render, reverse
 from django.contrib import messages
+from home.models import SocialIcon
 
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request, "Your booking is empty")
+        messages.info(request, "Your booking is empty")
         return redirect(reverse('lessons'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+    social = SocialIcon.objects.all()
 
     context = {
         'order_form': order_form,
+        'socials':social,
 
     }
 
