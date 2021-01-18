fd8a1bbbfeb0b7d2f16b92204aad6cd679b6ee00
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-18 12:08:50 +0000

    Save purchased order function added

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render, reverse
 from django.contrib import messages
 
 from checkout.forms import OrderForm
+from checkout.models import OrderLineItem
+from lessons.models import Lesson
 from home.models import SocialIcon
 from booking.contexts import booking_contents
 
@@ -13,26 +15,74 @@ def checkout(request):
     public_key = settings.STRIPE_PUBLIC_KEY
     secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.info(request, "Your booking is empty")
-        return redirect(reverse('lessons'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    currnet_bag = booking_contents(request)
-    total = currnet_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = secret_key
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'postcode': request.POST['postcode'],
+            'county': request.POST['county'],
+            'country': request.POST['country'],
+        }
 
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        order_form = OrderForm(form_data)
 
-    order_form = OrderForm()
+        if order_form.is_valid():
+            order = order_form.save()
+            for lesson_id, item_data in bag.items():
+                try:
+                    lesson = Lesson.objects.get(id=lesson_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            lesson=lesson,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        messages.error("Something whent wrong..")
+
+                except Lesson.DoesNotExist:
+                    messages.error(request, (
+                        "One of the lessons in your booking 'bag' wasn't found in our database. "
+                        "Please message us for assistance!"
+                    ))
+                order.delete()
+                return redirect(reverse('booking'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout-success', args=[order.order_number]))
+        else:
+            messages.error(request, "There was an error with your form. \
+                Check your information, please.")
+
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.info(request, "Your booking is empty")
+            return redirect(reverse('lessons'))
+
+        currnet_bag = booking_contents(request)
+        total = currnet_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = secret_key
+
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not public_key:
         messages.warning(
-            request, 'Stripe public key is missing. Check your environment, maybe it is there sleeping.')
+            request, 'Stripe public key is missing. \
+                Check your environment, maybe it is there sleeping.')
 
     template = 'checkout/checkout.html'
     social = SocialIcon.objects.all()
