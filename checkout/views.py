b231aed356e39a531ec620d273d4b7dea7d9186f
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-18 20:01:26 +0000

    Cache checkout data view added

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.views.decorators.http import require_POST
 from django.shortcuts import redirect, render, reverse, get_object_or_404
 from django.contrib import messages
 
@@ -9,6 +12,25 @@ from home.models import SocialIcon
 from booking.contexts import booking_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'We are sorry that your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpRequest(content=e, status=400)
 
 
 def checkout(request):
