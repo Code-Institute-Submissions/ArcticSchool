dcedead4450364c9bf5feab2be96e092a66c1d6e
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-19 12:06:50 +0000

    Quptes sing replaced to match

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('success/<order_number>',
          views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data,
-         name="cache_checkout_data"),
+         name='cache_checkout_data'),
     path('webhook/', webhook, name='webhook'),
 ]
