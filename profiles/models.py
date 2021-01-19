72bf87159c796fbe30156fab7c8f39e281bc7275
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-19 21:14:29 +0000

    User model created

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """ User Profile model to maintain current bookings, history of bookings """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_full_name = models.CharField(
+        max_length=50, null=False, blank=False)
+    default_email = models.EmailField(max_length=254, null=False, blank=False)
+    default_phone_number = models.CharField(
+       max_length=20, null=True, blank=True))
+    default_street_address1=models.CharField(
+        max_length = 80, null = True, blank = True)
+    default_street_address2=models.CharField(
+       max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(
+       max_length=40, null=True, blank=True}
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Select Country *', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.user.username
