7bb5216291df2580c0c475da0c70895a21226f18
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-19 21:21:57 +0000

    Wrong brackets error removed

--- profiles/models.py
+++ profiles/models.py
@@ -14,14 +14,14 @@ class UserProfile(models.Model):
         max_length=50, null=False, blank=False)
     default_email = models.EmailField(max_length=254, null=False, blank=False)
     default_phone_number = models.CharField(
-       max_length=20, null=True, blank=True))
+       max_length=20, null=True, blank=True)
     default_street_address1=models.CharField(
         max_length = 80, null = True, blank = True)
     default_street_address2=models.CharField(
        max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(
-       max_length=40, null=True, blank=True}
+       max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_country = CountryField(
         blank_label='Select Country *', null=True, blank=True)
