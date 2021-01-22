83e9777ce6c0808a10bd49ed9a96b1850b7e8717
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-22 18:09:45 +0000

    User details form model updated

--- profiles/models.py
+++ profiles/models.py
@@ -1,27 +1,39 @@
+""" Something """
 from django.db import models
-from django.contrib.auth.models import User
-from django_countries.fields import CountryField
-
+from django.db.models.fields import CharField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_countries.fields import CountryField
+
+from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
     """ User Profile model to maintain current bookings, history of bookings """
 
+    newsletter_choices = ((True, 'Yes'), (False, 'No'))
+
+    class Meta:
+        verbose_name_plural = 'Users Profiles'
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(
+    full_name = CharField(max_length=200, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(
         max_length=20, null=True, blank=True)
-    default_street_address1 = models.CharField(
+    street_address1 = models.CharField(
         max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(
+    street_address2 = models.CharField(
         max_length=80, null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(
         max_length=40, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_country = CountryField(
-        blank_label='Select Country *', null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(
+        blank_label='Select Country', null=True, blank=True)
+    receiving_newsletter = models.BooleanField(choices=newsletter_choices,
+                                               null=True, default=False)
 
     def __str__(self):
         return self.user.username
