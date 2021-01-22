7fb79ceebb9a17d14d955dde6b0ee59ba78c459e
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-22 21:53:20 +0000

    Administration list display added

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,10 +1,19 @@
+""" This module contains registered UserProfile administration """
 from django.contrib import admin
 from .models import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
+    """ Administartion display list for user """
     list_display = (
-        'first_name',
+        'full_name',
+        'email_address',
+        'phone_number',
+        'street_address1',
+        'town_or_city',
+        'postcode',
+        'country',
+        'receiving_newsletter',
     )
 
     ordering = ('first_name',)
