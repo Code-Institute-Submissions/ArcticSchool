273ec56386e5ede5a6752df6d71f78e151cb7bf9
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-22 18:08:35 +0000

    User profile app registered

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+    )
+
+    ordering = ('first_name',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
