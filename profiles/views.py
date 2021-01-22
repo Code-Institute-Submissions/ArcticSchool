ca533d08f6f8999754c5d09c6a0ed6d5820dff70
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-22 18:09:07 +0000

    Update user details post method added

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+"""
+This module will render user account page with
+all neccessery user information and forms
+"""
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from profiles.forms import UserDetailsForm
+from .models import UserProfile
+
+
 
 @login_required(login_url='/accounts/login/')
 def user_account(request):
     """ A view to return account page """
 
-    return render(request, 'account/account.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+
+    if request.method == 'POST':
+        form = UserDetailsForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile detailes updated successfully!')
+
+
+    details_form = UserDetailsForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'account/account.html'
+    context = {
+        'form': details_form,
+        'orders': orders,
+        'no_bag': True,
+    }
+    return render(request, template, context)
