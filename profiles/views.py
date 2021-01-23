cc174648b35cb73a431d5bf5b01e9e4861e64f6f
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-23 18:27:18 +0000

    Archived view created

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from profiles.forms import UserDetailsForm
 from home.models import SocialIcon
+from lessons.models import Lesson
+from checkout.models import Order
 from .models import UserProfile
 
 
@@ -25,12 +27,38 @@ def user_account(request):
     details_form = UserDetailsForm(instance=profile)
     orders = profile.orders.all()
     social = SocialIcon.objects.all()
+    last_booked = Lesson.objects.all()
 
     template = 'account/account.html'
     context = {
         'form': details_form,
         'orders': orders,
         'socials': social,
+        'last_booked': last_booked,
         'no_bag': True,
     }
     return render(request, template, context)
+
+
+def booking_archived(request, order_number):
+    """ A view to return User order history """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for oder number { order_number } \
+        A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout-success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
+
+
+def booking_active(request):
+    """ A view to return active bookings """
+    
+    return render(request, 'profile/account.html')
