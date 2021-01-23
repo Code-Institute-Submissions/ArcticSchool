6a3eb75e87938f322687feca0cb53d452c177700
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-23 18:30:04 +0000

    Commentaries added

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,13 +1,16 @@
+""" Checkout app Admin """
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
 class OrderInlineItemAdmin(admin.TabularInline):
+    """ Class to view a sngle lesson in admin order view """
     model = OrderLineItem
     readonly_fields = ('lineitem_total', 'quantity',)
 
 
 class OrderAdmin(admin.ModelAdmin):
+    """ Class to view a order in admin view """
     inlines = (OrderInlineItemAdmin,)
 
     readonly_fields = ('order_number', 'date',
@@ -15,12 +18,12 @@ class OrderAdmin(admin.ModelAdmin):
                        'grand_total', 'original_bag',
                        'stripe_pid',)
 
-    fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 'street_address1',
-              'street_address2', 'postcode', 'town_or_city',
-              'county',  'country', 'order_total',
-              'discount', 'grand_total', 'original_bag',
-              'stripe_pid',)
+    fields = ('order_number', 'user_profile', 'date',
+              'full_name', 'email', 'phone_number',
+              'street_address1', 'street_address2', 'postcode',
+              'town_or_city', 'county', 'country',
+              'order_total', 'discount', 'grand_total',
+              'original_bag', 'stripe_pid',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'discount',
