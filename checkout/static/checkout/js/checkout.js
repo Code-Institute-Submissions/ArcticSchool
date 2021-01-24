82ba0cd0800569ba11632ab9380cf6b88faf2ee3
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-24 19:23:00 +0000

    Prevent from purchasing when required fields are missing funciton added. Disable attribute added to submit form button

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -1,7 +1,11 @@
+$(document).ready(
+    // Run check all fields on document ready
+    checkAllFields()
+)
 /**
- * This funtcion will change Country field syle
- * when value is different than default
- */
+* This funtcion will change Country field syle
+* when value is different than default
+*/
 let countrySelected = $('#id_country').val();
 if (!countrySelected) {
     $('#id_country').css('color', '#ced4da');
@@ -15,15 +19,33 @@ $('#id_country').change(function () {
     }
 });
 
-
-$('.InputElement').bind('keyup', function () {
-    if (allFilled()) $('#submit-button').removeAttr('disabled');
-});
-
-function allFilled() {
-    let filled = true;
-    $('.InputElement').each(function () {
-        if ($(this).val() == '') filled = false;
+/**
+ * This function will check every required
+ * field and disable button if any of them is empty
+ */
+function checkAllFields() {
+    $('#payment-form input[required]').each(function () {
+        if ($(this).val() != '') {
+            $('#submit-button').prop('disabled', false);
+        }
+        else {
+            $('#submit-button').prop('disabled', true);
+            return false
+        }
     });
-    return filled;
 }
+/**
+ * This function will check input field on changed
+ * if and check all fields after that to make sure all required
+ * fielsd are properly filled in
+ */
+$('#payment-form input[required]').change(function () {
+    if ($(this).val() != '') {
+        $('#submit-button').prop('disabled', false);
+        checkAllFields();
+    }
+    else {
+        $('#submit-button').prop('disabled', true);
+        return false
+    }
+});
