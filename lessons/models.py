32c3c8096ea2cfea3560f641536c762797f61d24
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-01-29 14:11:53 +0000

    Models required fields changed/updated

--- lessons/models.py
+++ lessons/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=False)
 
     def __str__(self):
         return self.friendly_name
@@ -20,19 +20,19 @@ class Lesson(models.Model):
     """ Lesson Model """
     name = models.CharField(max_length=254)
     category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.PROTECT)
+        'Category', null=True, blank=False, on_delete=models.PROTECT)
     level = models.ForeignKey(
-        LevelCard, null=True, blank=True, on_delete=models.PROTECT)
+        LevelCard, null=True, blank=False, on_delete=models.PROTECT)
     description = models.TextField(null=True, blank=True)
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=False)
+    end_date = models.DateField(null=True, blank=False)
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
-    participants = models.IntegerField(null=True, blank=True)
+    participants = models.IntegerField(null=True, blank=False)
     resort = models.ForeignKey(
-        Resort, null=True, blank=True, on_delete=models.PROTECT)
+        Resort, null=True, blank=False, on_delete=models.PROTECT)
     price = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+        max_digits=6, decimal_places=2, null=True, blank=False)
     supper_offer = models.BooleanField(null=True, blank=True, default=False)
     image = models.ImageField(upload_to="lessons", null=True, blank=True)
 
--- team/models.py
+++ team/models.py
@@ -7,9 +7,9 @@ class InstructorProfile(models.Model):
     class Meta:
         verbose_name_plural = 'Instructors Profiles'
 
-    name = models.CharField(max_length=254)
-    age = models.CharField(max_length=2, null=True, blank=True)
-    about = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=254, blank=False)
+    age = models.CharField(max_length=2, null=True, blank=False)
+    about = models.TextField(null=True, blank=False)
     image = models.ImageField(upload_to="instructors",null=True, blank=True)
 
     def __str__(self):
